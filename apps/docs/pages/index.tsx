b966bd669fbd247c2c010eed38493c7c896fc5a1
Asadul islam <islamasadul327@gmail.com>
2022-10-29 13:25:32 +0600

    updates

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -4,7 +4,7 @@ export default function Docs() {
   return (
     <div>
       <h1>Hello first my application monorepo with terbo</h1>
-      <Button />
+      <Button>Docs</Button>
     </div>
   );
 }
--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
 
-export const Button = () => {
-  return <button>Boop</button>;
+export const Button = (props: any) => {
+  return <button style={props.className}>{props.button}</button>;
 };
